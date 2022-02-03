1f1f6a7a229d61cb8ec42b404648906b96f4624a
clague <lclague9@gmail.com>
2022-02-03 13:18:22 -0700

    adding scaffolding for connecting wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,165 @@
 import type { NextPage } from "next";
+import { useEffect, useState, useCallback, useReducer } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import twitterLogo from "../public/twitter-logo.svg";
 import { Heading, Card, Text, Button } from "degen";
 import Web3Modal from "web3modal";
+import { ethers, providers } from "ethers";
+import { NFTMembership, ActionType, StateType } from "../utils/types";
+import { getChainData, ellipseAddress } from "../utils/utilities";
 
 // misc imports:
 const TWITTER_HANDLE = "clague17";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-// Wallet stuff
-let web3Modal;
+const providerOptions = {};
+
+const subscriptions: Array<NFTMembership> = [
+  {
+    image: "https://picsum.photos/200", // random img endpoint
+    title: "Luis",
+    description: "This is an NFT membership!",
+    price: 20,
+  },
+  {
+    image: "https://picsum.photos/200", // random img endpoint
+    title: "Clague",
+    description: "This is an NFT membership! 2",
+    price: 20,
+  },
+];
+
+let web3Modal: Web3Modal;
 if (typeof window !== "undefined") {
   web3Modal = new Web3Modal({
     network: "mainnet", // optional
     cacheProvider: true,
+    providerOptions, // required
   });
 }
 
-type Subscription = {
-  title: string;
-  author: string;
-  coverImage: string;
+const initialState: StateType = {
+  provider: null,
+  web3Provider: null,
+  address: null,
+  chainId: null,
 };
 
-const subscriptions: Array<Subscription> = [
-  {
-    title: "Luis",
-    author: "LuisAuthor",
-    coverImage: "lol.jpeg",
-  },
-  { title: "L2", author: "L2Author", coverImage: "lol2.jpeg" },
-];
+function reducer(state: StateType, action: ActionType): StateType {
+  switch (action.type) {
+    case "SET_WEB3_PROVIDER":
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case "SET_ADDRESS":
+      return {
+        ...state,
+        address: action.address,
+      };
+    case "SET_CHAIN_ID":
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case "RESET_WEB3_PROVIDER":
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
 
 const Home: NextPage = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { provider, web3Provider, address, chainId } = state;
+
+  const connect = async function () {
+    // useCallback is nice because this function will be the same
+    // This is the initial `provider` that is returned when
+    // using web3Modal to connect. Can be MetaMask or WalletConnect.
+    const provider = await web3Modal.connect();
+
+    // We plug the initial `provider` into ethers.js and get back
+    // a Web3Provider. This will add on methods from ethers.js and
+    // event listeners such as `.on()` will be different.
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+
+    const network = await web3Provider.getNetwork();
+
+    dispatch({
+      type: "SET_WEB3_PROVIDER",
+      provider,
+      web3Provider,
+      address,
+      chainId: network.chainId,
+    });
+  };
+
+  const disconnect = async () => {
+    web3Modal.clearCachedProvider();
+    console.log("What's going on here?", provider);
+    if (provider?.disconnect && typeof provider.disconnect === "function") {
+      console.log("Are we supopsed to be here?");
+      await provider.disconnect();
+    }
+    dispatch({
+      type: "RESET_WEB3_PROVIDER",
+    });
+  };
+  // auto connect to the cached provider
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      connect();
+    }
+  }, [connect]);
+
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts: string[]) => {
+        // eslint-disable-next-line no-console
+        console.log("accountsChanged", accounts);
+        dispatch({
+          type: "SET_ADDRESS",
+          address: accounts[0],
+        });
+      };
+
+      // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+      const handleChainChanged = (_hexChainId: string) => {
+        window.location.reload();
+      };
+
+      const handleDisconnect = (error: { code: number; message: string }) => {
+        // eslint-disable-next-line no-console
+        console.log("disconnect", error);
+        disconnect();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      // Subscription Cleanup
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider, disconnect]);
+
+  const chainData = getChainData(chainId);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -59,7 +184,23 @@ const Home: NextPage = () => {
                   <Text variant="large">Buyer</Text>
                 </span>
                 <div className="m-auto">
-                  <Button>Connect Wallet</Button>
+                  {address && (
+                    <div className="grid">
+                      <div>
+                        <p className="mb-1">Network:</p>
+                        <p>{chainData?.name}</p>
+                      </div>
+                      <div>
+                        <p className="mb-1">Address:</p>
+                        <p>{ellipseAddress(address)}</p>
+                      </div>
+                    </div>
+                  )}{" "}
+                  {web3Provider ? (
+                    <Button onClick={disconnect}>Disconnect Wallet</Button>
+                  ) : (
+                    <Button onClick={connect}>Connect Wallet</Button>
+                  )}
                 </div>
               </div>
             </div>
@@ -74,7 +215,7 @@ const Home: NextPage = () => {
                 <div className="m-6">
                   <Card hover padding="6">
                     <Heading>{sus.title}</Heading>
-                    <Text>{sus.author}</Text>
+                    <Text>{sus.description}</Text>
                   </Card>
                 </div>
               ))}
